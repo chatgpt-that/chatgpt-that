9a0e71b00bd1b1307a4d7f5ab828099711726283
Sase <sasenarine.ramaischrand@umbrage.com>
2024-11-21 11:07:42 -0600

    feat: splash-nav ui-component

---
+++ packages/web-app/src/components/splash-nav/splash-nav.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export interface NavItem {
+  label: string;
+  onClick: () => void;
+}
+
+export interface SplashNavProps {
+  title: string;
+  logoPath: string;
+  navItems: NavItem[];
+}
+
+export function SplashNav({
+  title,
+  logoPath,
+  navItems,
+}: Readonly<SplashNavProps>) {
+  
+  return (
+    <nav className='w-full h-[120px] flex items-center justify-between border-b-2 bg-white'>
+      <div className='flex gap-4 items-center'>
+        <div 
+          style={{ background: `url(${logoPath})` }} 
+          className='size-[64px] rounded-full border-2 border-[#407BFF] !bg-center !bg-cover !bg-no-repeat hover:cursor-pointer'
+        />
+        <div className='font-[Gilroy] font-[900] text-3xl text-[#000000d9]'>{title}</div>
+      </div>
+      <div className='flex gap-6 items-center'>
+        { navItems.map((navItem) => (
+          <div 
+            key={navItem.label} 
+            className='font-[Gilroy] font-[600] text-lg text-[#000000d9] hover:cursor-pointer hover:underline hover:text-[#407BFF]'
+            onClick={() => navItem.onClick()}
+          >
+            { navItem.label }
+          </div>
+        )) }
+      </div>
+    </nav>
+  );
+
+}
